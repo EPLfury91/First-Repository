7e1a42a703e7a475bd0778ab315192e0936be400
Brett <eplfury@yahoo.com>
2022-10-04 07:43:28 -0400

    Initial Commit

---
+++ GitHub Practice/GitHub_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_PracticeApp.swift
+//  GitHub Practice
+//
+//  Created by Brett Tracey on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
